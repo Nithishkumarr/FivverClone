42b49f3b3418333838782f3b7e0e5da347ea954d
Nithishkumarr <nithishrajesh18me@nieit.ac.in>
2023-03-06 19:51:38 +0530

    Add Nav menu

---
+++ frontend/src/components/navbar/Navbar.jsx
@@ -0,0 +1,74 @@
+import { React, useState, useEffect } from "react";
+
+import "./Navbar.scss";
+const Navbar = () => {
+  const [active, setActive] = useState(false);
+  const [open, setOpen] = useState(false);
+  const isActive = () => {
+    window.scrollY > 0 ? setActive(true) : setActive(false);
+  };
+  useEffect(() => {
+    window.addEventListener("scroll", isActive);
+    return () => {
+      window.removeEventListener("scroll", isActive);
+    };
+  }, []);
+
+  const currentUser = {
+    id: 1,
+    userName: "john Doe",
+    isSeller: true,
+  };
+  return (
+    <div className={active ? "navbar active" : "navbar"}>
+      <div className="container">
+        <div className="logo">
+          <span className="text">fiver</span>
+
+          <span className="dot">.</span>
+        </div>
+        <div className="links">
+          <span>Fiver Business</span>
+          <span>Explore</span>
+          <span>English</span>
+          <span>Sign in</span>
+          {!currentUser?.isSeller && <span>Become a seller</span>}
+          {!currentUser && <button>Join</button>}
+          {currentUser && (
+            <div className="user" onClick={() => setOpen(!open)}>
+              <img
+                src="https://images.unsplash.com/photo-1606663889134-b1dedb5ed8b7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTh8fGNhcnRvb258ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60"
+                alt="cartoon"
+              />
+              <span>{currentUser?.userName}</span>
+              {open && (
+                <div className="options">
+                  {currentUser?.isSeller && (
+                    <>
+                      <span>Gigs</span>
+                      <span>Add New Gig</span>
+                    </>
+                  )}
+                  <span>Orders</span>
+                  <span>Messsages</span>
+                  <span>LogOut</span>
+                </div>
+              )}
+            </div>
+          )}
+        </div>
+      </div>
+      {active && (
+        <>
+          <hr />
+          <div className="menu">
+            <span>Test</span>
+            <span>Test2</span>
+          </div>
+        </>
+      )}
+    </div>
+  );
+};
+
+export default Navbar;
---
+++ frontend/src/pages/messages/Messsages.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import "./Messsages.scss";
+const Messsages = () => {
+  return <div>Messsages</div>;
+};
+
+export default Messsages;

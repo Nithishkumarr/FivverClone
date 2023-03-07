ea752e61b8149295b9628147fec6d10f6d56dbfb
Nithishkumarr <nithishrajesh18me@nieit.ac.in>
2023-03-07 18:34:44 +0530

    add infinite scroll using lib

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -1,7 +1,22 @@
 import React from "react";
+import Featured from "../../components/featured/Featured";
+import Slide from "../../components/slide/Slide";
+import TrustedBy from "../../components/trustedBy/TrustedBy";
 import "./Home.scss";
+import CatCard from "../../components/catCart/CatCard";
+import { cards } from "../../data";
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div>
+      <Featured />
+      <TrustedBy />
+      <Slide slidesToShow={5} arrowsScroll={5}>
+        {cards.map((card) => (
+          <CatCard item={card} key={card.id} />
+        ))}
+      </Slide>
+    </div>
+  );
 };
 
 export default Home;
